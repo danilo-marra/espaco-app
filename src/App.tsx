a03fe09b87371128db91ddb5b92839469583799b
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-05-27 10:11:48 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function App() {
+
+
+  return (
+   <h1 className='text-3xl font-bold underline'>
+    Espaço Dialógico
+   </h1>
+  )
+}
+
+
